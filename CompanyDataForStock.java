9aeb654590e7a779f73d629edf2d41188cec13c5
pooja16 <poojarajwar3@gmail.com>
2017-04-10 15:10:57 +0530

    Required Company Data for Stock

---
+++ CompanyDataForStock.java
@@ -0,0 +1,16 @@
+package com.Bridgelabz.javaPrograms;
+
+public class Company 
+{
+	String compName;
+	int noOfShare;
+	float sharePrice;
+	
+	Company(String compName, int noOfShare, float sharePrice) 
+	{
+		this.compName = compName;
+		this.noOfShare = noOfShare;
+		this.sharePrice = sharePrice;
+	}
+	
+}
